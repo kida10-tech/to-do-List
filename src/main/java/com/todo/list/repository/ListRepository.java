135fa4f69014babce1bf29b203396d72354820a3
Diego Martinez Giardini <diego.martinez.giardini@gmail.com>
2022-01-05 17:42:41 -0300

    Main branch cleared

--- src/main/java/com/todo/list/repository/ListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.todo.list.repository;
-
-import com.todo.list.model.ListEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ListRepository extends JpaRepository<ListEntity, Long> {
-}
